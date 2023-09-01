76bca5a5c5dbac96bb4e8c66a86cd742e4746e68
Joe <joaoguibil@gmail.com>
2023-09-01 01:14:12 -0300

    Nada util

--- PrimeiraLista/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package PrimeiraLista;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Ola Mundo");
-        System.out.println("Programa Acabou");
-    }
-}
