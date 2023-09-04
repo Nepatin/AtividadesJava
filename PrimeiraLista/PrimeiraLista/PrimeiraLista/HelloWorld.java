3d27d1dba35f979fe7cb9e6da1b5430706321c7e
Joe <joaoguibil@gmail.com>
2023-09-04 00:00:14 -0300

    Hello World em Java
    Básico.

---
+++ PrimeiraLista/PrimeiraLista/PrimeiraLista/HelloWorld.java
@@ -0,0 +1,9 @@
+package PrimeiraLista;
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Ola Mundo");
+        System.out.println("Programa Acabou");
+    }
+}
