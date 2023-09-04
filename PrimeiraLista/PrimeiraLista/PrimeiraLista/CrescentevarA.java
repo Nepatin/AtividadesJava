ee6889f4e99ffb201ffda328e1917c5bad889271
Joe <joaoguibil@gmail.com>
2023-09-04 18:57:25 -0300

    Crescente usando if else if
    Faça um algoritmo que leia três números e imprima-os em ordem crescente.
    OBS: No final há outras maneiras de fazer o algoritmo na linguagem C, mas fazendo algumas alterações é possível aplicar no Java.

--- PrimeiraLista/PrimeiraLista/PrimeiraLista/CrescentevarA.java
+++ PrimeiraLista/PrimeiraLista/PrimeiraLista/CrescentevarA.java
@@ -8,45 +8,44 @@ public class CrescentevarA {
 
         Scanner num = new Scanner (System.in);
 
-        System.out.println("Ola, bem vindo ao meu programa.\nIrei ler tres numeros inteiros e irei mostrar em ordem crescente.");
+        System.out.println("Ola, bem vindo ao meu programa.\nIrei ler tres numeros inteiros e vou imprimir em ordem crescente.");
 
-        System.out.println("Digite o primeiro numero:");
+        System.out.println("\nDigite o primeiro numero:");
         a = num.nextInt();
 
-        System.out.println("Digite o segundo numero:");
+        System.out.println("\nDigite o segundo numero:");
         b = num.nextInt();
 
-        System.out.println("Digite o terceiro numero:");
+        System.out.println("\nDigite o terceiro numero:");
         c = num.nextInt();
 
         if ((a <= b && b <= c))
         {
-        System.out.printf("A ordem crescente: %d %d %d\n", a, b, c);
+            System.out.printf("\n%d, %d, %d\n", a, b, c);
         }
         else if ((a <= c && c <= b))
         {
-        System.out.printf("A ordem crescente: %d %d %d\n", a, c, b);
+            System.out.printf("\n%d, %d, %d\n", a, c, b);
         }
         else if ((b <= a && a <= c))
         {
-        System.out.printf("A ordem crescente: %d %d %d\n", b, a, c);
+            System.out.printf("\n%d, %d, %d\n", b, a, c);
         }
         else if ((b <= c && c <= a))
         {
-        System.out.printf("A ordem crescente: %d %d %d\n", b, c, a);
+            System.out.printf("\n%d, %d, %d\n", b, c, a);
         }
         else if ((c <= a && a <= b))
         {
-        System.out.printf("A ordem crescente: %d %d %d\n", c, a, b);
+            System.out.printf("\n%d, %d, %d\n", c, a, b);
         }
         else if ((c <= b && b < a))
         {
-        System.out.printf("A ordem crescente: %d %d %d\n", c, b, a);
+            System.out.printf("\n%d, %d, %d\n", c, b, a);
         }
         
-        System.out.println("Fim do programa.");
+        System.out.println("\nFim do programa.");
 
         num.close();
-        
     }
 }
