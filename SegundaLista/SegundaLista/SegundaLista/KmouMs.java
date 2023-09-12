9eaa7cfbc1def341760e2b94e2cd7a4576e9cf7b
Joe <joaoguibil@gmail.com>
2023-09-12 00:03:25 -0300

    Conversão de Km para Ms ou vice-versa
    Codigo para converter km para ms ou vice e versa usando comando de repetição Do

---
+++ SegundaLista/SegundaLista/SegundaLista/KmouMs.java
@@ -0,0 +1,40 @@
+package SegundaLista.SegundaLista.SegundaLista;
+
+import java.util.Scanner;
+
+public class KmouMs {
+    public static void main(String[] args) {
+        
+        float num, c;
+        int a, b = 0;
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Olá, bem vindo ao meu programa.\n Irei fazer a conversao de Km/s para M/s ou vice-versa.");
+
+        do {
+            System.out.println("Digite a velocidade:");
+            num= input.nextFloat();
+
+            System.out.println("Converver Km/h para M/s digite 1\n Converter M/s para Km/h digite 2");
+            a= input.nextInt();
+
+            if(num == 0 || a == 2) {
+                c = (float) (num * 3.6);
+                System.out.printf("A conversao de %.2f para Km/h eh %.2f\n", num, c);
+
+                System.out.println("Para sair digite -1, para continuar digite 3");
+                b = input.nextInt();
+                
+            } else if (num == -1 || a == 1 ) {
+                c = (float) (num / 3.6);
+
+                System.out.printf("A conversao de %.2f para M/s eh %.2f\n", num, c);
+
+                System.out.println("Para sair digite -1, para continuar digite 3");
+                b = input.nextInt();
+            }
+        } while (b != -1);
+        input.close();
+    }
+}
